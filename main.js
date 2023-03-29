f92799312d9f54becedc086f1de353d173002d0a
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2023-03-28 19:28:13 -0500

    Modificacion de la tematica

--- main.js
+++ main.js
@@ -6,3 +6,36 @@ addEventListener("submit", async(e)=>{
     let datos = await peticion.text();
     document.querySelector("pre").innerHTML = datos;
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')        
+}
