f47b7720f62ca08f95aaeff76f00981feaa00b4c
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2023-04-01 10:14:25 -0500

    Web terminada

--- api.php
+++ api.php
@@ -1,6 +1,42 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    $_DATA = json_decode(file_get_contents("php://input"), true);
+    $data = json_decode(file_get_contents("config.json"), true);
+    $json = (object) [];
+    $json->header = ["<li><a href='#'>".$_SERVER['HTTP_HOST']."</a></li>"];
+    $json->section = (object) ["pagrahp" => null, "image"=> null];
+    $json->nav = [];
+    $json->footer = [];
 
-    echo json_encode($_DATA,JSON_PRETTY_PRINT);
+    //Header
+    foreach ($data["header"] as $key => $value) {
+        array_push($json->header, <<<HTML
+        <li><a href='{$value["href"]}' target="_blank">{$value["name"]}</a></li>
+        HTML);
+    }
+    $json->header = join(" ", $json->header);
+    //Section
+    $json->section->pagrahp = <<<HTML
+        <h2>{$data["section"]["pagrahp"]["title"]}</h2>    
+        <p>{$data["section"]["pagrahp"]["pagrahp"]}</p>
+        <a href='{$data["section"]["pagrahp"]["btn"]["href"]}' target="_blank">{$data["section"]["pagrahp"]["btn"]["name"]}</a>
+    HTML;
+    $json->section->image = <<<HTML
+        <img src='{$data["section"]["image"]["img"]}' class="starbucks">
+    HTML;
+    //Nav
+    foreach ($data["nav"] as $key => $value){
+        array_push($json->nav, <<<HTML
+        <li><a href='{$value["href"]}' target="_blank"><img src='{$value["src"]}'></a></li>
+        HTML);
+    }
+    $json->nav = join(" ", $json->nav);
+    //Footer
+    foreach ($data["footer"] as $key => $value){
+        array_push($json->footer, <<<HTML
+        <li><img src='{$value["src"]}' onclick='imgSlider("{$value["src"]}");changeCircleColor("{$value["color"]}")'></li>
+        HTML);
+    }
+    $json->footer = join(" ",$json->footer);
+
+    echo json_encode($json,JSON_PRETTY_PRINT);
 ?>
--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-addEventListener("submit", async(e)=>{
-    e.preventDefault(); 
-    let data = Object.fromEntries(new FormData(e.target));
-    document.querySelectorAll('[type="checkbox"]').forEach((val, id)=> (val.checked) ?((typeof(data[`${val.name}`])=="string") ? data[`${val.name}`] = [val.value] : data[`${val.name}`].push(val.value)):null);
-    let peticion = await fetch(e.target.action,{method: e.target.method, body: JSON.stringify(data)});
-    let datos = await peticion.text();
-    document.querySelector("pre").innerHTML = datos;
+addEventListener("DOMContentLoaded", async(e)=>{
+    // https://instructormiguel.000webhostapp.com/Actividad_Pagina_Web_25_03_2023/api.php
+    let peticion = await fetch("https://instructormiguel.000webhostapp.com/Actividad_Pagina_Web_25_03_2023/api.php");
+    let datos = await peticion.json();
+    document.querySelector(".navigation").innerHTML = datos.header;
+    document.querySelector(".textBox").innerHTML = datos.section.pagrahp;
+    document.querySelector(".imgBox").innerHTML = datos.section.image;
+    document.querySelector(".thumb").innerHTML = datos.footer;
+    document.querySelector(".sci").innerHTML = datos.nav;
 })
+
+
+
+
+
 
 
 
